a9e75df7c20a86e87f009292e7a25406ed35158b
Claudia Nathasia Jason,Claudia C <c.jason@student.fontys.nl>
2018-11-20 13:10:48 +0100

    commit from claudia

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="flex-center position-ref full-height">
-    Hello World!
+    Hello Worldd!!
     <a href="{{ url('welcome') }}">Welcome!</a>
     This is my first Laravel.
 </div>
